b63e4d565dbb0f91701163a3726265d1264bef47
server <server@sorin11.campus.ad.csulb.edu>
2017-08-07 18:21:10 -0700

    Test to send print command (thought I broke ICM).

--- ICM_Test.pl
+++ ICM_Test.pl
@@ -3,7 +3,7 @@
 
 $fileinfo = "\nICM_Test.pl last updated 07-01-17\n";
 
-############  define I/O && initialize
+# Define I/O && initialize
 $info = "\nICM_Test.pl loads in multiple files into ICM. It will need the full directory path as well as the file(s) count.";
 
 $input = "\nUsage\:  ICM_Test.pl  [options]\n
@@ -11,11 +11,12 @@ $input = "\nUsage\:  ICM_Test.pl  [options]\n
 \t-d   \t\tFull Path to the files
 \t-n   \t\tNumber of files to load
 ";
-# set default values that can be overwritten #
-$directory = $ENV{'PWD'};;
+# Set default values that can be overwritten #
+$directory = $ENV{'PWD'};
 $files_Number = 0;
-$icm_home = "/home/server/ICM_Test_Folder"
-# get flags #
+$icm_home = "/home/server/icm-3.7-2b/";
+
+# Get flags #
 if((@ARGV)) {
   @options = @ARGV;
   for ($i=0; $i<=$#ARGV; $i++) {
@@ -35,10 +36,17 @@ if($help==1){
   print "$input\n"; exit();
 }
 
-#Create ICM script
-open(ICM,'>',$icm_home."temp.icm") || die "Please give me output filename $!"; #adjust the ICMscript 
-print ICM "#!$icm_home"."icm -s\n";
+# Create ICM script
+open(ICM,'>',$directory."temp.icm") || die "Please give me output filename $!"; #adjust the ICMscript 
+#print ICM "#!$icm_home"."icm64 \n";
+#print ICM "openFile '/home/server/ICM_Test_Folder/bbutyl_top_50/bbutyl_dock1213.ob'\n";
+#print ICM "quit\n";
 close(ICM)||die $!;
-#Ending creating ICM script
+# Ending creating ICM script
 
+# Running the command to load in files
+chdir $icm_home;
+$temp_Directory = $directory."temp.icm";
+system("./icm64 -g $temp_Directory"); 
+# End running the command
 ###################################################################################################################################################################################
