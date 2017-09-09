90c4ee700d8f27e8baa7bcfd1d32893135501028
Xavier Martinez <xavier.martinez@student.csulb.edu>
2017-09-08 21:44:29 -0700

    Major enhancements (using abolute paths instead of counter) and error checking.

--- generate-rmsd-rg-fah.pl
+++ generate-rmsd-rg-fah.pl
@@ -9,57 +9,122 @@
 
 # This script extract RMSD and Rg values for the F@H data.
 # Type `usegromacs???`, where ??? stands for GROMACS version, before running this script
+# Lastly, this script must be run from with a Project<#> folder.
 
-$usage = "perl  script.pl  [project]  [ndx file]  [starting structure] [g_rms least squares group flag] [g_rms RMSD group flag] [g_gyrate group flag]
+$usage = "perl  script.pl  [project]  [ndx file]  [starting structure] [g_rms least squares group flag #] [g_rms RMSD group flag #] [g_gyrate group flag #] [RMSD, RG, or both? (1,2,3) optional] [frame 0 only? (1) optional]
 NOTE: ndx and starting structure files must have absolute paths";
 # ------------------------------------------------------------------------------------
 # GET ARGUMENTS
 	$project                     = $ARGV[0] or die "$usage\n";
 	$ndx                         = $ARGV[1] or die "$usage\n";
 	$nativeStrture               = $ARGV[2] or die "$usage\n";
-	$g_rms_least_squares_flag    = $ARGV[3] or die "$usage\n";
-	$g_rms_RMSD_calculation_flag = $ARGV[4] or die "$usage\n";
-	$g_gyrate_group_flag         = $ARGV[5] or die "$usage\n";
-
+	$g_rms_least_squares_flag    = int($ARGV[3]) or die "$usage\n";
+	$g_rms_RMSD_calculation_flag = int($ARGV[4]) or die "$usage\n";
+	$g_gyrate_group_flag         = int($ARGV[5]) or die "$usage\n";
+	$g_rms_g_gyrate_switch       = int($ARGV[6]) or 3;
+	$frame_0_only_switch         = int($ARGV[7]) or 0;
 
 # ------------------------------------------------------------------------------------
 # ?
-	$num_runs = &count_directory("RUN");
+	if ($g_rms_g_gyrate_switch < 0 || $g_rms_g_gyrate_switch > 3) {
+		die "$usage\n";
+	}
+	
+	if ($frame_0_only_switch < 0 || $frame_0_only_switch > 1) {
+		die "$usage\n";
+	}
+
+	$currentDir = `pwd`; chomp $currentDir;
+	@runs = &pattern_walk("RUN", $currentDir);
+	$num_runs = scalar(@runs);
 	print "Number of runs is $num_runs\n";
-	$currentDir = `pwd`; chomp($currentDir);
 	mkdir "RMSD-RG";
-
-	for (my $run=0; $run<$num_runs; $run++){
-		# go to a run dir and count number of clones
-		chdir "RUN$run";
-		my $num_clones = &count_directory("CLONE");
-		print "RUN$run: $num_clones clones\n";
-
-		for (my $clone=0; $clone<$num_clones; $clone++){
-			# go to each clone and run g_rms & g_gyrate
-			chdir "CLONE$clone";
-			print "Working on PROJ$project/RUN$run/CLONE$clone...\n";
-
-			my $rmsd_output = "P$project"."_R$run"."_C$clone"."_rmsd.xvg";
-			my $gyrate_output = "P$project"."_R$run"."_C$clone"."_gyrate.xvg";
-			my $xtc = "P$project"."_R$run"."_C$clone".".xtc";
-			`echo 1 1 | g_rms -s $nativeStrture -f $xtc -n $ndx -o $rmsd_output`;
-			`echo 1 | g_gyrate -s $nativeStrture -f $xtc -n $ndx -o $gyrate_output`;
-			
-			my $RMSD_dir = "$currentDir"."/RMSD-RG";
-			`mv $rmsd_output $RMSD_dir`;
-			`mv $gyrate_output $RMSD_dir`;
-
-			chdir ".."; # go out of CLONE* directory
-		} # END OF LOOP THROUGH CLONE* directories
-		chdir ".."; # go out of RUN* directory
+	$out_dir = "$currentDir"."/RMSD-RG";
+	foreach $run (@runs){
+		if (-d $run) {
+			if ($frame_0_only_switch == 1) {
+				$num_clones = 1;
+				print "$run: $num_clones clones\n";
+				@clones[0] = "$run/CLONE0";
+			} else {
+				@clones = &pattern_walk("CLONE", $run);
+				$num_clones = scalar($clones);
+				print "$run: $num_clones clones\n";
+			}
+			foreach $clone (@clones){
+				if (-d $clone) {
+					@rc = &get_rc($clone);
+					$run_number = $rc[0];
+					$clone_number = $rc[1];
+					if ($frame_0_only_switch == 1) {
+						$rmsd_output = "$out_dir/P$project"."_R$run_number"."_C$clone_number"."_F0_rmsd.xvg";
+						$gyrate_output = "$out_dir/P$project"."_R$run_number"."_C$clone_number"."F0_gyrate.xvg";
+						@files = &pattern_walk("f0.pdb", $clone);
+						$num_files = scalar(@files); 
+						if ($num_files > 1) {
+							die "[ERROR] More than one ..f0.pdb file at $clone. Exiting...";
+						} elsif ($num_files == 0) {
+							die "[ERROR] No ..f0.pdb file at $clone. Exiting...";
+						}
+						$data_file = $files[0];
+					} else {
+						$rmsd_output = "$out_dir/P$project"."_R$run_number"."_C$clone_number"."_rmsd.xvg";
+						$gyrate_output = "$out_dir/P$project"."_R$run_number"."_C$clone_number"."_gyrate.xvg";
+						$data_file_name = "P$project"."_R$run_number"."_C$clone_number".".xtc";
+						@files = &pattern_walk($data_file_name, $clone);
+						$num_files = scalar(@files); 
+						if ($num_files > 1) {
+							die "[ERROR] More than one $data_file_name at $clone. Exiting...";
+						} elsif ($num_files == 0) {
+							die "[ERROR] No $data_file_name file at $clone. Exiting...";
+						}
+						$data_file = $files[0];
+					}
+					if ($g_rms_g_gyrate_switch == 1) {
+						`echo $g_rms_least_squares_flag $g_rms_RMSD_calculation_flag | g_rms -s $nativeStrture -f $data_file -n $ndx -o $rmsd_output`;
+					} elsif ($g_rms_g_gyrate_switch == 2) {
+						`echo $g_gyrate_group_flag | g_gyrate -s $nativeStrture -f $data_file -n $ndx -o $gyrate_output`;
+					} else {
+						`echo $g_rms_least_squares_flag $g_rms_RMSD_calculation_flag | g_rms -s $nativeStrture -f $data_file -n $ndx -o $rmsd_output`;
+						`echo $g_gyrate_group_flag | g_gyrate -s $nativeStrture -f $data_file -n $ndx -o $gyrate_output`;
+					}
+				} else {
+					print "[ERROR] $clone does not exist. Exiting...";
+				}
+			} # END OF LOOP THROUGH CLONE* directories
+		} else {
+			print "[WARNING] $run does not exist. Skipping...";
+		}
 	} # END OF LOOP THROUGH RUN* directories
 
-	#close OUTPUT;
-
-sub count_directory{ # arguments: pattern in directory names
-	local($pattern,@num_dir);
+# Requires tree Linux OS command
+sub pattern_walk{ # arguments: pattern to search for, absolute path of directory
+	local($pattern, $path, @dirs, $num_dirs);
 	$pattern = $_[0];
-	@num_dir = `tree -i -L 1 | grep $pattern`;
-	return scalar(@num_dir);
+	$path = $_[1];
+	@dirs = `ls $path | grep $pattern`;
+	$num_dirs = scalar(@dirs);
+	for (my $x=0; $x<$num_dirs; $x++) {
+		$dir = $dirs[$x];
+		chomp $dir;
+		$dirs[$x] = "$path/$dir";
+	}
+	return @dirs;
+}
+sub get_rc{ # arguments: absolute path of directory
+	local($path, @rc);
+	$path = $_[0];
+	@split = split("/", $path);
+	foreach $val (@split) {
+		if (index($val, "RUN") != -1) {
+   			$val =~ s/RUN//ig;
+			chdir ".."; # go out of RUN* directory
+		@rc[0] = $val;
+		}
+		if (index($val, "CLONE") != -1) {
+   			$val =~ s/CLONE//ig;
+			@rc[1] = $val;
+		}
+	}
+	return @rc;
 }
